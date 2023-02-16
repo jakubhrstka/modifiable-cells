e2e3216aa7d475142b2c37972d49b3d28960b482
Jakub Hrstka <jakubhrstkaa@gmail.com>
2023-02-16 19:04:59 +0100

    removed normalize.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Cells from "./components/Cells/Cells";
 
 function App() {
   return (
-    <main className="text-white bg-slate-800 h-screen w-screen flex flex-col justify-center items-center">
+    <main className="text-white bg-slate-800 h-screen w-screen flex flex-col justify-center items-center gap-2">
       <h1>modifiableCells</h1>
       <Cells />
     </main>
